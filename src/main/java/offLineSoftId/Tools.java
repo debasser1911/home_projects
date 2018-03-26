a1474a09b3986e391f2515917d748ad203c94989
debasser1911 <iexu2stl>
2018-03-26 12:02:08 +0300

    proper softid

--- src/main/java/offLineSoftId/Tools.java
+++ src/main/java/offLineSoftId/Tools.java
@@ -6,30 +6,36 @@ import java.nio.charset.Charset;
 
 public class Tools {
     public static void getData() throws IOException, InterruptedException {
-        Process softID = new ProcessBuilder("D:\\offLineSoftId\\offLineSoftId.exe").start();
 
-        InputStream errorStream = softID.getErrorStream();
-        /*Process soft = Runtime.getRuntime().exec("D:\\offLineSoftId");*/
+        //TODO absolute path
+        //Start process
+        ProcessBuilder softID = new ProcessBuilder("D:\\offLineSoftId\\offLineSoftId.exe");
+        //Set working directory
+        softID.directory(new File("D:\\offLineSoftId\\"));
+        //Process start
+        Process soft = softID.start();
+        //Create new log file
         File file = new File("D:\\xxx.xml");
-        /*File file = new File("D:\\xxx.txt");*/
-        BufferedReader br = new BufferedReader(new InputStreamReader(softID.getInputStream(), Charset.forName("CP866")));
+        //Init reader and writer
+        BufferedReader br = new BufferedReader(new InputStreamReader(soft.getInputStream(), Charset.forName("UTF8")));
         BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-
-
+        //Init data string
         String line;
 
+        System.out.println("Getting data");
 
+        //Getting data
         while ((line = br.readLine()) != null) {
             bw.write(line);
             bw.flush();
             System.out.println(line);
-
-            /*if (line.contains("offLineError")){
-                System.out.println("Get error");
-                break;
-            }*/
         }
-        softID.waitFor();
+
+        soft.waitFor();
         br.close();
+        bw.close();
     }
 }
+
+
+
--- src/main/java/offLineSoftId/offLineSoftId.java
+++ src/main/java/offLineSoftId/offLineSoftId.java
@@ -1,6 +1,6 @@
 package offLineSoftId;
 
-import java.io.*;
+import java.io.IOException;
 
 public class offLineSoftId {
     public static void main(String[] args) throws IOException, InterruptedException {

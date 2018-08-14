c4df87c93e9a6b4735a98a6de231fb27a7549628
DeBasser <debasser1911@gmail.com>
2018-08-14 19:47:21 +0300

    organize files

--- src/main/java/qatest.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by DeBasser on 30.08.2017.
- */
-public class qatest {
-    public static void main(String[] args) {
-        System.out.println("Ярик, красава, поздравляю!");
-    }
-}
